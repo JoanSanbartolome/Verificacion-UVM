9981e12989e73392d47949864b19114332e8f7c5
JoanSanbartolome <josanbart@gmail.com>
2026-09-22 12:13:46 +0200

    dev: extension de clase para el llenado de la fifo

--- VsCodeQuestasim-main/src/RCSG_subir.sv
+++ VsCodeQuestasim-main/src/RCSG_subir.sv
@@ -2,6 +2,7 @@ class RCGS_subir extends RCGS_base #(
     parameter int WIDTH = 32,
     parameter int DEPTH = 16
 );
+    constraint llenar {rd_en dist { 1 := 70, 0 := 30};};
 
     function new();
         super.new();
